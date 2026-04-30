7883f31182e9c5e09a9f668eeeecd975b2d51e0d
Muhammad Azzami Yahya <4zzyhy@gmail.com>
2026-04-30 18:47:49 +0700

    feat(product services): added structure for every entity (category, product, productImage, productReview)

---
+++ services/service-2/app/Models/ProductReview.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductReview extends Model
+{
+    //
+    protected $fillable = [
+        'product_id', 'user_id', 'rating', 'comment'
+    ];
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
