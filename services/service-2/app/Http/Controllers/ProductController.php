5585c741cbf02eb7de0bb2b8f9cc8641fc8a5663
Muhammad Azzami Yahya <4zzyhy@gmail.com>
2026-05-01 00:18:41 +0700

    feat(product services): added a controller

---
+++ services/service-2/app/Http/Controllers/ProductController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //
+    protected $productService;
+
+    public function __construct(ProductService $productService)
+    {
+        $this->productService = $productService;
+    }
+
+    public function index(Request $request)
+    {
+
+        $filters = [
+            'category_id' => $request->query('category_id'),
+            'search' => $request->query('search')
+        ];
+        
+        $perPage = $request->query('per_page', 10);
+
+        $products = $this->productService->getAllProducts($filters, $perPage);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Daftar produk berhasil diambil',
+            'data' => $products
+        ], 200);
+    }
+}
